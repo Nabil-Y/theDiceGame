7603e975631705cc885228a7c52b89720a8be9b5
Nabil-Y <91381714+Nabil-Y@users.noreply.github.com>
2021-11-10 13:23:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function rollDice() {
+    var firstDice = Math.trunc(Math.random()*6)+1;
+    var secondDice = Math.trunc(Math.random()*6)+1;
+
+    document.querySelector("img.img1").setAttribute("src", "./images/dice"+firstDice+".png");
+    document.querySelector("img.img2").setAttribute("src", "./images/dice"+secondDice+".png");
+
+    if ( firstDice > secondDice) { 
+        document.querySelector("h1").textContent = "Player 1 wins!";
+    } else if ( secondDice > firstDice) {
+        document.querySelector("h1").textContent = "Player 2 wins!";
+    } else {
+        document.querySelector("h1").textContent = "Draw";
+    }
+
+}
